b064c5934b991667b2f247c73b4c5db4327b5ff0
HoeSeong123 <ghltjd3@gmail.com>
2024-08-18 19:25:32 +0900

    test

--- backend/src/main/java/codezap/CodeZapApplication.java
+++ backend/src/main/java/codezap/CodeZapApplication.java
@@ -3,10 +3,14 @@ package codezap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class CodeZapApplication {
 
     public static void main(String[] args) {
+        log.info("새로운 파일을 실행합니다.");
         SpringApplication.run(CodeZapApplication.class, args);
     }
 
